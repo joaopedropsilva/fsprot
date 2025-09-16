68be1984564c059ed5b92182911d38ce2417e0bf
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2025-09-16 06:47:45 -0300

    make app installation by user

--- setup.sh
+++ setup.sh
@@ -1,34 +1,31 @@
 #!/usr/bin/env bash
 
-INSTALL_PATH=/usr/local/share/fsprot
+INSTALL_PATH=$HOME/.fsprot
 
 echo [fsprot-setup] Installing app at $INSTALL_PATH
-# overrides everything, change install path to
-# private user path or deny this behaviour
-sudo rm -rf $INSTALL_PATH
-sudo mkdir $INSTALL_PATH
-sudo cp -r ./src/* $INSTALL_PATH
-sudo chmod --recursive 0644 $INSTALL_PATH
-sudo chmod 0755 $INSTALL_PATH
+rm -rf $INSTALL_PATH
+mkdir -p $INSTALL_PATH/src
+cp -r ./src/* $INSTALL_PATH/src
+chmod 0644 $INSTALL_PATH/src/*.{py,c}
 
 echo [fsprot-setup] Installing app dependencies
-sudo python3 -m venv $INSTALL_PATH/venv
-sudo $INSTALL_PATH/venv/bin/python3 -m pip install -r ./requirements.txt
-
-echo [fsprot-setup] Creating app executable
-cat <<END_SCRIPT | sudo tee $INSTALL_PATH/fsprot > /dev/null
-#!/usr/bin/bash
-
-INSTALL_PATH=$INSTALL_PATH
-\$INSTALL_PATH/venv/bin/python3 \$INSTALL_PATH/cli.py "\$@"
-END_SCRIPT
-sudo chmod +x $INSTALL_PATH/fsprot
+python3 -m venv $INSTALL_PATH/venv
+$INSTALL_PATH/venv/bin/python3 -m pip install -r ./requirements.txt
 
 echo [fsprot-setup] Compiling scripts
 # Add compilation with -D flag
-sudo gcc -o $INSTALL_PATH/cap $INSTALL_PATH/cap.c
-sudo rm $INSTALL_PATH/cap.c
-sudo setcap cap_dac_override,cap_dac_read_search+ep $INSTALL_PATH/cap
+gcc -o $INSTALL_PATH/src/cap $INSTALL_PATH/src/cap.c
+rm $INSTALL_PATH/src/cap.c
+sudo setcap cap_dac_override,cap_dac_read_search+ep $INSTALL_PATH/src/cap
+
+echo [fsprot-setup] Creating app executable
+cat <<END_SCRIPT > $INSTALL_PATH/fsprot
+#!/usr/bin/env bash
+
+INSTALL_PATH=$INSTALL_PATH
+\$INSTALL_PATH/venv/bin/python3 \$INSTALL_PATH/src/cli.py "\$@"
+END_SCRIPT
+chmod +x $INSTALL_PATH/fsprot
 
 echo [fsprot-setup] Adding CLI to PATH
 USER_SHELL=$(getent passwd "$USER" | cut -d: -f7)
